0c83122caf222bed8270167a9bf933546c63a760
linzell <linzellart@gmail.com>
2022-07-17 23:22:00 +0200

    Update

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -40,6 +40,40 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if team_1_score.clone() == team_2_score {
+            scores.insert(team_1_name.clone(), Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+            scores.insert(team_2_name.clone(), Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        } else if team_1_score.clone() > team_2_score {
+            scores.insert(team_1_name.clone(), Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+            scores.insert(team_2_name.clone(), Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        } else {
+            scores.insert(team_1_name.clone(), Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+            scores.insert(team_2_name.clone(), Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
+        }
     }
     scores
 }
